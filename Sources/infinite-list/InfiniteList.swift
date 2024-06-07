145d6a200b9278bd7fa2fb7e4d15bf52d60b9228
Alexander Shvets <33634796+shvets-semandex@users.noreply.github.com>
2024-06-07 13:27:49 -0400

    simplify classes

--- Sources/infinite-list/InfiniteList.swift
+++ Sources/infinite-list/InfiniteList.swift
@@ -1,40 +1,33 @@
 import SwiftUI
-//import Introspect
 
 public struct InfiniteList<Item, Content>: View where Item: Identifiable & Hashable, Content: View {
   var name: String
   var columnsCount: Int
   var dataSource: DataSource<Item>
-  var content: (Item) -> Content
+  var itemContent: (Item) -> Content
 
-  public init(name: String, columnsCount: Int = 1, dataSource: DataSource<Item>, content: @escaping (Item) -> Content) {
+  public init(name: String, columnsCount: Int = 1, dataSource: DataSource<Item>, itemContent: @escaping (Item) -> Content) {
     self.name = name
     self.columnsCount = columnsCount
     self.dataSource = dataSource
-    self.content = content
+    self.itemContent = itemContent
   }
 
   public var body: some View {
     listItems()
       .navigationTitle(name)
-      .modifier(InfiniteListStyleModifier())
   }
 
   private func listItems() -> some View {
-    GeometryReader { geometry in
       ScrollView {
         LazyVGrid(columns: Array(repeating: .init(), count: columnsCount)) {
           ForEach(dataSource.items) { item in
-            content(item)
+            itemContent(item)
               .onAppear {
                 dataSource.loadNext(item)
               }
           }
         }
-//          .introspectTableView { tableView in
-//            //print(tableView)
-//            tableView.separatorStyle = .singleLineEtched
-//          }
           .padding(.horizontal)
           .refreshable {
             dataSource.loadFirst()
@@ -48,11 +41,6 @@ public struct InfiniteList<Item, Content>: View where Item: Identifiable & Hasha
                 .scaleEffect(1.5, anchor: .center)
             }
           }
-        }
-//        .introspectScrollView { scrollView in
-//          scrollView.refreshControl = UIRefreshControl()
-//        }
-        .frame(width: geometry.size.width, height: geometry.size.height-20, alignment: .center)
     }
   }
 }
